ff42759b9deb58f6d58f4bfc0e425e8b56fa4c40
Sameer Singh <Sameer0Singh123@gmail.com>
2024-07-29 03:08:18 +0530

    Added the button logic for manipulating the name or input fied when edit button is clicked

--- src/component/boardContainer/PlayerInfo.jsx
+++ src/component/boardContainer/PlayerInfo.jsx
@@ -3,12 +3,14 @@ import { useState } from "react";
 export default function PlayerInfo({button, playerName, playerSymbol}) {
   const [isEditing, setIsEditing] = useState(false);
   
+//   function for updating the state with the help of this maniputing playerName span element with input field
   function selectHandler(){
       console.log("hello world")
       setIsEditing(true)
       console.log(isEditing)
   }
-
+  
+//   logic for showing span instead of input or vise vera for is isEditing is ture or false
   const PlayerInfo = isEditing ? (<input type ="text" required/>) : ( <span className="player-name">{playerName}</span>)
   return (
     <li>

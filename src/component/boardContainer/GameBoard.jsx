d54f97002ebaabf9c642cc7e660cb7a1228d5d98
Sameer Singh <Sameer0Singh123@gmail.com>
2024-07-31 01:43:23 +0530

    We have added click functionality in the game and further we are going to add more logic to make it playable

--- src/component/boardContainer/GameBoard.jsx
+++ src/component/boardContainer/GameBoard.jsx
@@ -1,3 +1,5 @@
+import {useState} from 'react'
+
 const board = [
   [null, null, null],
   [null, null, null],
@@ -5,16 +7,34 @@ const board = [
 ];
 
 export default function GameBoard() {
+
+//   Adding the click functionality to our gameboard/
+//  and updating the state whenever the userclick on the cell and reflecting the result with an x
+  const [gameBoard, upDategameBoard] = useState(board);
+   
+  function clickHandler(rowIndex, colIndex){
+       upDategameBoard((previousGameBoard) =>{
+        // we have created a copy of initialBoard because it is a good practice  
+        // to avoid the bugs and etx
+
+        const newGameBoard = [...previousGameBoard.map((innerCell)   =>{
+           return  [...innerCell]  // we have used the map method to render the inner cell of the board
+        })]
+        newGameBoard[rowIndex][colIndex] = 'X';  
+        return newGameBoard;
+       })
+  }
+
   return (
     <ol id="game-board">
-      {board.map((row, index) => {
+      {gameBoard.map((row, rowindex) => {
         return (
-          <li key={index}>
+          <li key={rowindex}>
             <ol>
-              {row.map((col, index) => {
+              {row.map((col, colindex) => {
                 return (
-                  <li key={index}>
-                    <button>{col}</button>
+                  <li key={colindex}>
+                    <button onClick={() => clickHandler(rowindex, colindex)}>{col}</button>
                   </li>
                 );
               })}
